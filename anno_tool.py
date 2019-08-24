5d8152d35fbe92fbab65654da9559376f14eecd9
Jake-T-Sullivan <53870502+Jake-T-Sullivan@users.noreply.github.com>
2019-08-23 17:05:49 -0700

    used carriage return trick for multiline

--- anno_tool.py
+++ anno_tool.py
@@ -2,18 +2,24 @@ import arcpy
 
 mxd = arcpy.mapping.MapDocument("CURRENT")
 df = arcpy.mapping.ListDataFrames(mxd)[0]
+df2 = arcpy.mapping.ListDataFrames(mxd)[1]
 lyr = arcpy.mapping.ListLayers(df)[0]
 
-wlist = [["HARNEY"], ["GRANT"], ["BAKER"]]
-vlist = [["1945"], ["1776"], ["1969"]]
+wlist = ["HARNEY", "GRANT", "BAKER"]
+vlist = ["1945", "1776", "1969"]
+
+
+def get_well_term(lyr2, well_col, attrib_col):
+    with arcpy.da.UpdateCursor(lyr2, "["+ +"]"")
+
+
 
 def Replace_Text(lyr,well_list,value_list):
     with arcpy.da.UpdateCursor(lyr, ["TextString"]) as cursor:
         for well, value in zip(well_list, value_list):
-        
-            new_text = ("""%s
-%s""" %(well,value,))
+
             for row in cursor:
+                new_text = well + "\r\n" + value
                 if well in row[0]:
                 
                     print("found IT!!!")
